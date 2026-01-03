01fd9d80640b89ac5575f4088b7d48112622636b
dongbao66 <95880227+dongbao66@users.noreply.github.com>
2026-01-03 22:23:04 +0800

    隐藏搜索框

--- worker-3.js
+++ worker-3.js
@@ -659,6 +659,7 @@ const HTML_CONTENT = `
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
         border: 1px solid #e0e0e0;
         transition: all 0.3s ease;
+        display: none !important;
     }
 
     .search-bar:focus-within {
